695ff5e39a3561871da6a8a299c4ba23c8cca55b
digitaltangent <info@digitaltangent.com>
2022-11-20 11:09:20 +0300

    Changes to connection

--- _config.js
+++ _config.js
@@ -1,9 +1,11 @@
 var config = {}
 
 // Update to have your correct username and password
+/** 
 config.mongoURI = {
     production: 'mongodb+srv://aleeda:VZKyrwuGmsGoGRF2@cluster0.4is5duk.mongodb.net/?retryWrites=true&w=majority',
     development: 'mongodb+srv://aleeda:VZKyrwuGmsGoGRF2@cluster0.4is5duk.mongodb.net/?retryWrites=true&w=majority',
     test: 'mongodb+srv://aleeda:VZKyrwuGmsGoGRF2@cluster0.4is5duk.mongodb.net/?retryWrites=true&w=majority',
 }
 module.exports = config;
+**/
